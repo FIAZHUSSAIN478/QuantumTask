e1429f0bcab1b1f34e1dcf16678187ae6cb1dd3c
Fiaz Hussain <fiaz.dev478@gmail.com>
2026-03-16 15:59:27 +0500

    Fix deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header1 from "./components/Header1";
 import Hero1 from "./components/Hero1";
-import Section1 from "./components/Section1";
 import Section2 from "./components/sections/Section2";
+
 
 function App() {
   return (
